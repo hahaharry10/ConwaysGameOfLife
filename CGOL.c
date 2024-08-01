c827e6922d43d78f9ae590dbed0b7f8eba38f21d
Harry Nixon <sc21hn@leeds.ac.uk>
2024-08-01 23:59:23 +0100

    Add initial blueprints of the functions required to create the game.

---
+++ CGOL.c
@@ -0,0 +1,22 @@
+#include "CGOL.h"
+
+void runSimulation(int** cellGrid) {
+}
+
+int** initCellGrid() {
+    int** cellGrid;
+    return cellGrid;
+}
+
+void freeCellGrid(int** cellGrid) {
+}
+
+void printCellGrid(int** cellGrid) {
+}
+
+int main( int argc, char** argv ) {
+    char* hello;
+
+    hello = argv[1];
+    return 0;
+}
