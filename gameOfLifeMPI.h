3a8dff4bac01e3b4dc7b3d16a7ea999426d0124e
Harry Nixon <hnixon@hahaharry.dev>
2024-08-13 16:17:23 +0100

    Change function declarations to match source file.

--- gameOfLifeMPI.h
+++ gameOfLifeMPI.h
@@ -4,7 +4,8 @@
 #define CELL_GRID_WIDTH 20
 #define CELL_GRID_HEIGHT 20
 
-void runSimulation(char* cellGrid); /* Run the simulation. */
+void updateBoundaryDomain( char* domain, int domainSize, int rank ); /* Update domain for boundary ranks (ranks that contain the first few or last few cells. */
+void updateCentreDomain( char* domain, int domainSize ); /* Update domain for non-boundary ranks. */
 char* initCellGrid(void); /* Allocate and initialise the cellGrid. */
 void freeCellGrid(char* cellGrid); /* Free allocated memory used for the cell grid. */
 void printCellGrid(char* cellGrid); /* Output the cell grid to the terminal. */
