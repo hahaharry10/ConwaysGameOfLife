df10427b6b129ab1c4dd7fc9188be2ded45035fb
Harry Nixon <hnixon@hahaharry.dev>
2024-08-15 03:19:04 +0100

    Add structure creators in header file.

--- gameOfLifeMPI.h
+++ gameOfLifeMPI.h
@@ -10,4 +10,10 @@ char* initCellGrid(void); /* Allocate and initialise the cellGrid. */
 void freeCellGrid(char* cellGrid); /* Free allocated memory used for the cell grid. */
 void printCellGrid(char* cellGrid); /* Output the cell grid to the terminal. */
 
+void createToad(char* cellGrid, int start_r, int start_c);
+void createBeacon(char* cellGrid, int start_r, int start_c);
+void createBlock(char* cellGrid, int start_r, int start_c);
+void createGlider(char* cellGrid, int start_r, int start_c);
+void createLightweightSpaceship(char* cellGrid, int start_r, int start_c);
+
 #endif
