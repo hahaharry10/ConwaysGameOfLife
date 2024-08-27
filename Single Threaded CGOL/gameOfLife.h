6a58b64b188e1a631d249760959b2acaed801038
Harry Nixon <hnixon@hahaharry.dev>
2024-08-27 19:20:38 +0100

    keep macros in GUI.h.

--- Single Threaded CGOL/gameOfLife.h
+++ Single Threaded CGOL/gameOfLife.h
@@ -1,9 +1,6 @@
 #ifndef CGOL_H
 #define CGOL_H
 
-#define CELL_GRID_WIDTH 20
-#define CELL_GRID_HEIGHT 20
-
 void runSimulation(char** cellGrid); /* Run the simulation. */
 char** initCellGrid(void); /* Allocate and initialise the cellGrid. */
 void freeCellGrid(char** cell); /* Free allocated memory used for the cell grid. */
